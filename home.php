2ede7421792078184acc0d422b995e1909dc2469
Denis Schuele <schueledenis@gmx.at>
2018-06-30 00:56:02 +0200

    fixed login bug

--- home.php
+++ home.php
@@ -1,25 +1,47 @@
-  <?php  
-        include('includes/header.php');
+<?php  
+    include('includes/header.php');
 
-        include('config/pdocon.php');
-        include('includes/functions.php');
+    include('config/pdocon.php');
+    include('includes/functions.php');
 
-        if(isset($_SESSION['user_is_logged_in'])){
-            $fullname = $_SESSION['user_data']['fullname'];
-           ?>
+    if(isset($_SESSION['user_is_logged_in'])){
+        $fullname = $_SESSION['user_data']['fullname'];
+        ?>
+            <div class="container">
                 <div class="card">
-   
+
                     <small class="pull-right">Hello <?= $fullname?> </small>
                 </div>
-           <?php
+        <?php
             echo "Sesseion";  
         } else{
             echo "Session not loged in !!!!";
         }
         
         ?>
-        <div class="msg"><?php showmsg(); ?></div>
+                <div class="msg"><?php showmsg(); ?></div>
+                <div class="row">
+                    <?php
+                        //Create a card for each car
+   
+                        $db->query('    SELECT * FROM `cars`
+                                        LEFT JOIN `locations`
+                                            ON `cars`.`fk_location_id` = `locations`.`id`
+                                        LEFT JOIN `car_descriptions` 
+                                            ON `cars`.`fk_car_description_id` = `car_descriptions`.`id`');
 
-  
+                        $results = $db->fetchMultiple();
+ 
+                            
+                        //Fetch all data and keep in a result set
+                        $row = $db->fetchSingle();
+                        if($row){
+                            echo "data";
+                        }
+                    ?>
+                </div>
+    
+            </div>
+
  
     
---
+++ img/login.php
@@ -0,0 +1,85 @@
+
+<?php
+ob_start();
+session_start();
+//Include functions
+include('../includes/functions.php');
+
+?>
+
+ 
+<?php
+
+//require or include your database connection file
+//require database class files
+require('../config/pdocon.php');
+    
+//instatiating our database objects
+$db = new Pdocon;
+
+    //Collect and process Data on login submission
+if(isset($_POST['submit_login'])){
+    
+    $raw_username       =   cleandata($_POST['username']);
+    
+    $raw_password       =   cleandata($_POST['password']);
+    
+    
+    $c_username         =   valemail($raw_username);            
+    
+    $hashed_password    =   hashpassword($raw_password);
+      
+    
+    $db->query('SELECT * FROM users WHERE email=:email AND `password`= :password');
+    
+    $db->bindValue(':email', $c_username, PDO::PARAM_STR);
+    $db->bindValue(':password',$hashed_password, PDO::PARAM_STR);
+    
+    $row = $db->fetchSingle();
+    
+    
+    if($row){
+        
+        $d_image        =   $row['img'];
+        
+        $d_name         =   $row['full_name'];
+        
+        
+        $_SESSION['user_data'] = array(
+        
+        
+        'fullname'      =>   $row['fullname'],
+        'id'            =>   $row['id'],
+        'email'         =>   $row['email'],
+
+        );
+        $_SESSION['user_is_logged_in']  =  true;
+        
+        redirect('../home.php');
+        
+        
+        keepmsg('<div class="alert alert-success text-center">
+                      <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                      <strong>Welcome </strong>' . $d_name . ' you are logged in! 
+                </div>');
+        
+        
+        
+        
+    }else{
+        redirect('../index.php?login');
+         keepmsg('<div class="alert alert-danger text-center">
+              <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+              <strong>Sorry!</strong> User does not exist. Register or Try Again
+            </div>');
+
+    }    
+    
+    
+    
+    
+}
+ 
+
+?>
+  
